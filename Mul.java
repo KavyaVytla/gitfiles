8501b6add1084dfc45709e81ab80ecdf9e9e1467
KavyaVytla <121013212+KavyaVytla@users.noreply.github.com>
2022-12-19 14:45:58 +0530

    Add files via upload

---
+++ Mul.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class Mul
+{
+ public static void main(String...args)
+ {
+   int n=2;
+   for(int i=1;i<=10;i++)
+   {
+     System.out.println(n+"*"+i+"="+n*i);
+   }
+ }
+}
